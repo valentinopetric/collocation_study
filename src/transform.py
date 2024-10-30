f9e55de7ff6e36db34db1aa1fac892284657ab7e
tetejac <teoterzic99@gmail.com>
2024-10-30 16:00:46 +0100

    air wings started

--- src/transform.py
+++ src/transform.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import json
 import os
+import requests
 
 def load_json(path):
     
@@ -31,4 +32,76 @@ def transform_diff_avg(df:pd.DataFrame):
               
     return diff_averages_df
     
+
+def get_device_data(device_id,indoor=True,start_date = '2024-10-16 05:00:00',end_date = '2024-10-21 00:00:00'):
     
+    if indoor:
+        r = requests.get(f"https://airwings-europe.wings-ict-solutions.eu/api/realvaluesindoor?", headers={"x-api-key": "804t9Tges45YyXUJ6GF4gfds45aretEW43w"}, params={"raw": "true",
+                    "start": start_date,
+                    "stop": end_date,
+                    "device_id": device_id
+                })
+    else:
+        r = requests.get(f"https://airwings-europe.wings-ict-solutions.eu/api/realvalues?", headers={"x-api-key": "804t9Tges45YyXUJ6GF4gfds45aretEW43w"}, params={"raw": "true",
+                    "start": start_date,
+                    "stop": end_date,
+                    "device_id": device_id
+                })
+        
+    if len(r.text) < 10:
+        return 
+    
+    file_text = json.loads(r.text)
+    df = pd.DataFrame(file_text)
+    
+    return df
+
+def get_all_devices_data(device_ids,indoor=True,start_date = '2024-10-16 05:00:00',end_date = '2024-10-21 00:00:00'):
+    
+    df_full = pd.DataFrame()
+
+    for device in device_ids:
+        df_device = get_device_data(device,indoor=indoor,start_date=start_date,end_date=end_date)
+        df_full = pd.concat([df_full,df_device],axis=0)
+    
+    return df_full
+    
+
+def transform_columns_air_w(df,indoor=True):
+    
+    if indoor:
+        new_column_names = {
+            'timestamp': 'datetime',
+            'timezone': 'timezone',
+            'CO2': 'co2',
+            'TVOC': 'tvoc',
+            'CO': 'co',
+            'PM1': 'pm_1.0',
+            'PM25': 'pm_2.5',
+            'PM10': 'pm_10',
+            'atmospheric_pressure': 'pressure',
+        }
+        df['sensor'] = 'air_wings_indoor'
+    else:
+        new_column_names = {
+            'timestamp': 'datetime',
+            'CO2': 'co2',
+            'TVOC': 'tvoc',
+            'CO': 'co',
+            'PM1': 'pm_1.0',
+            'PM25': 'pm_2.5',
+            'PM10': 'pm_10',
+            'atmospheric_pressure': 'pressure',
+            'NO2': 'no2',
+            'O3': 'o3',
+            'SO2': 'so2',
+            'NO': 'no',
+        }
+        df['sensor'] = 'air_wings_outdoor'
+        
+    df.rename(columns=new_column_names, inplace=True)
+    df.drop(['timezone'],axis=1,inplace=True)
+    df['datetime'] = pd.to_datetime(df['datetime'])
+    df.set_index('datetime', inplace=True)
+    
+    return df
