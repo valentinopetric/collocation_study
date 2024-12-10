ccd19d1dcf1f58aa8f0cd67dbcf0ad701cbd27fc
Valentino Petric <101862974+valentinopetric@users.noreply.github.com>
2024-12-10 17:01:16 +0100

    downloading of json file with experiments information

--- Data_downloading.R
+++ Data_downloading.R
@@ -1,10 +1,12 @@
 # downloading the data from google drive
 if(!require(googledrive)) install.packages("googledrive")
+if(!require(jsonlite)) install.packages("jsonlite")
 library(googledrive)
+library(jsonlite)
 
 drive_deauth()
 drive_user()
-public_file <-  drive_get(as_id("1WBgBC8wsSceg2SMNNlH3oD-FzVRJB3_9"))
+public_file <-  drive_get(as_id("1WBgBC8wsSceg2SMNNlH3oD-FzVRJB3_9")) # This is id of dataset
 drive_download(public_file, overwrite = TRUE)
 
 # unzipping the downloaded file
@@ -19,3 +21,13 @@ for (csv_file in csv_files) {
   saveRDS(data, rds_file)
 }
 
+# downloading the json file 
+if(!dir.exists("data")) dir.create("data")
+public_file_json <- drive_get(as_id("1YLzoKvg8GLVW32Ybykw2eBaN2k-lKKoi")) # This is id of json file
+destination <- "data/experiments.json"
+drive_download(public_file_json, path = destination, overwrite = TRUE)
+
+#loading the json file
+experiments_data <- fromJSON(destination)
+
+print(experiments_data)
